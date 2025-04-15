b83c18f7ec331495ade5cebef679688e8386f70d
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-16 01:15:43 +0530

    Add additional test log statements to index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+
+console.log("This is a test file.");
+console.log("This is a test file 2.");
+console.log("This is a test file 3.");
+console.log("This is a test file 4.");
+console.log("This is a test file 5.");
+console.log("This is a test file 6.");
+
