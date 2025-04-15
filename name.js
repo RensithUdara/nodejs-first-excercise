2a96eb33f881a50ae6b71501430eda20e4f134e9
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-04-16 02:14:34 +0530

    Fix console log to correctly parse and display command line argument for location

--- name.js
+++ name.js
@@ -7,5 +7,5 @@ function printName(name) {
 // );
 
 console.log("My name is "+ process.argv[2]+".");
-console.log("I am from "+ process.argv[3]+"."); 
+console.log("I am from "+ passInt(process.argv[3])+"."); 
 console.log("I am "+ process.argv[4] + " years old."); 
